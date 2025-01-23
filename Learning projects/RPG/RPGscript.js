32bcbbb85b67431970c0279424ab147834bc90be
Jordan Wylie <jordandwylie@gmail.com>
2025-01-23 11:53:11 -0600

    Had a double up on the function. Fixed this to let button work correctly

--- Learning projects/RPG/RPGscript.js
+++ Learning projects/RPG/RPGscript.js
@@ -51,19 +51,11 @@ function update(location) {
 }
 
 function goTown() {
-  function goTown() {
-    update(locations[0]);
-  }
+  update(locations[0]);
 }
 
 function goStore() {
-  button1.innerText = "Buy 10 health (10 gold)";
-  button2.innerText = "Buy weapon (30 gold)";
-  button3.innerText = "Go to town square";
-  button1.onclick = buyHealth;
-  button2.onclick = buyWeapon;
-  button3.onclick = goTown;
-  text.innerText = "You enter the store.";
+  update(locations[1]);
 }
 
 function goCave() {
